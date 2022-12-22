0936ff0c2fbb54aa2a4aa471fc3b88b061c838b6
matuljain <matul3jan@gmail.com>
2022-12-22 10:36:49 +0000

    Remove express server

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
     transpileDependencies: [
         'vuetify'
     ],
+    publicPath: process.env.NODE_ENV === 'production' ? 'dist' : '',
     devServer: {
         allowedHosts: 'all',
         proxy: {
